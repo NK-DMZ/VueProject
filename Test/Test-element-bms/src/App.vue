49a408f20b107e7ddc5a23f4352321d2372f63c0
NK-DMZ <2206040225@qq.com>
2021-07-08 00:31:09 +0800

    test

---
+++ Test/Test-element-bms/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <hr>
+    <el-button type="danger"></el-button> -->
+    <Home></Home>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import Home from './components/home/home';
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+@import"./assets/css/base.css";
+</style>
---
+++ Test/Test-element-bms/src/components/home/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <el-container class="container">
+          <el-header class="header">
+              <Headdemo></Headdemo>
+          </el-header>
+          <el-main>
+              main
+          </el-main>
+          <el-footer>footer</el-footer>
+      </el-container>      
+  </div>
+</template>
+
+<script>
+import Headdemo from '../header/header.vue'
+export default {
+  components: { Headdemo },
+
+}
+</script>
+
+<style>
+.container{
+    /* border: 1px blue solid; */
+}
+.el-header{
+    padding: 0;
+}
+</style>
